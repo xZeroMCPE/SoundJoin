1fafbce905c82f7cbc124a333544bc766558e6e1
Andre <andrethegamer911@gmail.com>
2016-02-05 18:56:01 -0800

    Error line 17 fixed

--- src/SoundJoin/Main.php
+++ src/SoundJoin/Main.php
@@ -7,11 +7,14 @@ use BukkitPE\event\Listener;
 use BukkitPE\event\player\PlayerJoinEvent;
 use BukkitPE\level\sound\Sound
 use BukkitPE\level\sound\PopSound
+use BukkitPE\utils\Config;
 
 class Events extends PluginBase implements Listener{
 
 public function onEnable() {
 $this->getServer()->getPluginManager()->registerEvents($this, $this);
+       $this->saveDefaultConfig();
+        $this->getLogger()->info("[SoundMessage] Plugin has been enabled");
 }
 
 public function onJoin(PlayerJoinEvent $event){
