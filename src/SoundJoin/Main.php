452f53a3cb702ce7052c5b7a4c7cdd65393dead7
Andre <andrethegamer911@gmail.com>
2016-02-05 18:43:30 -0800

    JoinMesage has been added

--- src/SoundJoin/Main.php
+++ src/SoundJoin/Main.php
@@ -1,1 +1,19 @@
-// created
+<?php
+
+namespace SoundJoin;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
+
+class Events extends PluginBase implements Listener{
+
+public function onEnable() {
+$this->getServer()->getPluginManager()->registerEvents($this, $this);
+}
+
+public function onJoin(PlayerJoinEvent $event){
+       $message = $this->getConfig()->get("JoinMessage") // So it can pull the users config (Message) via config ;) 
+$event->getPlayer->sendMessage("$JoinMessage");
+}
+}
