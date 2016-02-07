1b52eadd12add8e6dbe49bd17ecd482153ba9113
Mr. Piggy <mcpepig123@gmail.com>
2016-02-06 17:01:03 -0800

    Learn PHP.

--- src/SoundJoin/Main.php
+++ src/SoundJoin/Main.php
@@ -13,20 +13,20 @@ use BukkitPE\level\sound\EndermanTeleportSound;
 use BukkitPE\level\sound\DoorSound;
 use BukkitPE\level\sound\BatSound;
 
-class Events extends PluginBase implements Listener{
+class Main extends PluginBase implements Listener{
 
 public function onEnable() {
-$this->getServer()->getPluginManager()->registerEvents($this, $this);
-      $this->config = (new Config($this->getDataFolder()."config.yml", Config::YAML))->getAll();
-       $this->saveDefaultConfig();
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->config = (new Config($this->getDataFolder()."config.yml", Config::YAML))->getAll();
+        $this->saveDefaultConfig();
         $this->getLogger()->info("[SoundMessage] Plugin has been enabled");
 }
 
 public function onJoin(PlayerJoinEvent $event){
-       $JoinMessage = $this->getConfig()->get("JoinMessage"); // So it can pull the users config (Message) via config :)
+        $JoinMessage = $this->getConfig()->get("JoinMessage"); // So it can pull the users config (Message) via config :)
         $Sound =$this->getConfig()->get("Sound");  // So it can pull the sound the user wants
         $player = $event->getPlayer();
-        $event->getPlayer->sendMessage("$JoinMessage");
+        $event->getPlayer->sendMessage($JoinMessage);
         $player->getLevel()->addSound(new $Sound($player), [$player]);
 }
 }
